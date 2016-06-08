c1a75e1234220817efbf7e216f6d5011dfad4ad2
fanxh <fanxh@thundersoft.com>
2016-06-08 10:00:07 +0800

    Add daily report table and combine with UI

--- zebu/apps/report/models.py
+++ zebu/apps/report/models.py
@@ -3,3 +3,8 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class ReportTable(models.Model):
+    product = models.CharField(max_length=50)
+    spm = models.CharField(max_length=50)
+    daily_reporter = models.CharField(max_length=50)
+    file_link = models.TextField()
--- zebu/apps/report/views.py
+++ zebu/apps/report/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
+from models import ReportTable
 
 # Create your views here.
 def reportPage(request):
-    return render(request, 'report/report.html')
-	
+    daily_report_tab = ReportTable.objects.all()
+    return render(request, 'report/report.html', {"daily_report_tab": daily_report_tab})
+    #return render(request, 'report/report.html')
+
 def report_Resource(request):
     return render(request, 'report/resource_usage.html')
 
 def report_MainTF(request):
     return render(request, 'report/main_tf_status.html')
-	
+
 def report_Schedule(request):
     return render(request, 'report/schedule.html')
