f5c9bf5eaeb8f95266d8795c33e30e52d50a17ee
fanxh <fanxh@thundersoft.com>
2016-09-02 12:35:19 +0800

    Add script to populate resource list into resource table

---
+++ zebu/populate_resource.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+#coding:utf-8
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "zebu.settings")
+
+'''
+Django 版本大于等于1.7的时候，需要加上下面两句
+import django
+django.setup()
+否则会抛出错误 django.core.exceptions.AppRegistryNotReady: Models aren't loaded yet.
+'''
+
+import django
+if django.VERSION >= (1, 7):#自动判断版本
+    django.setup()
+
+
+def main():
+    from apps.newhome.models import ResourceTable
+    f = open('resource_list.txt')
+
+    for line in f:
+        name,city = line.split(',')
+        #print '#####################'
+        #print name,city
+        ResourceTable.objects.get_or_create(resource_id=name,city=city)
+
+    f.close()
+
+if __name__ == "__main__":
+    main()
+    print('Done!')
