d4eaac86c0bb64db3cd3946fe3085bd499756345
ellen <ellen.yang@spreadtrum.com>
2016-09-11 19:54:08 +0800

    change planpage sendmail

--- zebu/apps/plan/views.py
+++ zebu/apps/plan/views.py
@@ -804,17 +804,17 @@ def ajaxpost(request):
         loginUser = request.POST['userInfo']
         receivers = [owner+'@spreadtrum.com','nicole.wang@spreadtrum.com','chunsi.he@spreadtrum.com','chunji.chen@spreadtrum.com','fiona.zhang@spreadtrum.com','xinpeng.li@spreadtrum.com','guoliang.ren@spreadtrum.com','ellen.yang@spreadtrum.com']
         #receivers = [owner+'@spreadtrum.com']
-        content = "Dear Owner:<br><br>zebu资源使用状态已更改，请知悉并及时处理，信息如下，<br>申请人："+owner+"<br>操作者："+loginUser+"<br>TF case："+tf_case+"Request Zebu Assign ID:"+assign_ID+"<br><br>管理系统地址：<a href='http://10.5.2.62'>http://10.5.2.62;</a><br>登录方式为外网域帐号。"
+        content = "Dear Owner:<br><br>zebu资源使用状态已更改，请知悉并及时处理，信息如下，<br>操作者："+loginUser+"<br>TF case："+tf_case+"Request Zebu Assign ID:"+assign_ID+"<br><br>管理系统地址：<a href='http://10.5.2.62'>http://10.5.2.62;</a><br>登录方式为外网域帐号。"
         #content = '被处理的TF case:'+tf_case+'/被分配使用的时间:'+edit_stime+'/status:'+statusAfter+'/操作者:'+loginUser
         subject = 'zebu资源的使用状态已更改，请登录指定服务器查看'
-        '''
+        
         #use sendEmailTest.send_mail() at Nanjing thundersoft site, use sendEmail.send_mail() at spreadtrum site
-        #if sendEmail.send_mail(subject, content, receivers):
-        if sendEmailTest.send_mail(subject,content,receivers):
+        if sendEmail.send_mail(subject, content, receivers):
+        #if sendEmailTest.send_mail(subject,content,receivers):
             print "send success"
         else:
             print"send fail"
-        '''
+        
     success_dict = {'edit_stime':edit_stime,
     'request_dura':request_dura,
     'daily_dura':daily_dura,
