b07a384205a0efa126e9ee581f85350dacdd8181
shiyh0122 <shiyh0122@thundersoft.com>
2016-09-26 13:50:22 +0800

    move pagnation of request page

--- zebu/apps/request/views.py
+++ zebu/apps/request/views.py
@@ -253,12 +253,12 @@ def requestUser(request, **kwargs):
             otherpara += "&order=" + request.GET.get("order")
 
         request_tab = RequestTable.objects.filter(project__in=prodtlist, acceptance__in=acceptlist).order_by(order)
+        count=request_tab.count()
 
         perpage = 15 #show how many items per page
     
         objects = request_tab
         pager = Paginator(objects,perpage)
-        num_pages = pager.num_pages
         try:
             projects = pager.page(gopage)
 
@@ -268,43 +268,34 @@ def requestUser(request, **kwargs):
             projects = pager.page(pager.num_pages)
         
         # Pagination range
-        # pages_left: how many page numbers show on left of current page at most
-        # pages_right: how many page numbers show on right of current page at most
-        pages_left = 4
-        pages_right = 4
-        current_page = gopage
-        range_left = "norange"
-        range_right = "norange"
-        over_range_left = "false"
-        over_range_right = "false"
+        page_show = 4
+        current_page = int(gopage)
+        page_count = pager.num_pages
+        page_status =  ""
 
-        #Range Left
-        if (int(gopage)-1 > pages_left):
-            over_range_left = "true"
-            range_left = range(int(gopage) - pages_left, int(gopage))
-        if (int(gopage)-1 == pages_left):
-            range_left = range(1, int(gopage))
-        if (int(gopage)-1 < pages_left):
-            range_left = range(1, int(gopage))
-        
-        #Range Right
-        if (int(gopage) < pager.num_pages - pages_right):
-            over_range_right = "true"
-            range_right = range(int(gopage) + 1, int(gopage) + 1 + pages_right)
-        if (int(gopage) == pager.num_pages - pages_right):
-            range_right = range(int(gopage) + 1, int(gopage) + 1 + pages_right)
-        if (int(gopage) > pager.num_pages - pages_right):
-            range_right = range(int(gopage) + 1, pager.num_pages + 1)
+        if page_count <= page_show + 1:
+            page_status = ""
+        else:
+            if current_page > 0 and current_page <= page_show:
+                page_status = "Left"
+
+            elif current_page > page_show  and page_count - current_page > page_show -1:
+                page_status = "Mid"
+
+            else:
+                page_status = "Right"
         
         return render(request, 'request/request.html', {
             "request_tab": projects, 
             'valid_duration': valid_duration, 
-            'range_left': range_left, 
-            'range_right': range_right, 
-            'over_range_left': over_range_left, 
-            'over_range_right': over_range_right,
             "productlist": productlist,
             "acceptancelist":acceptancelist,
+            "page_count": page_count,
+            "page_show_range": range(page_show+1),
+            "page_range": range(page_count),
+            "page_status": page_status,
+            "page_right_range": range(page_count-page_show, page_count+1),
+            "count":count,
             "filter": filter,
             "order": order,
             "otherpara": otherpara,
